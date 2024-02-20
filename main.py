d8fdd25b4260fd2a5fdcae51d57a1cd58f614654
Matwej Novgorodtsev <92621652+mnovgorodtsev@users.noreply.github.com>
2024-02-20 13:25:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-test_url='https://steamcommunity.com/market/listings/730/London%202018%20Legends%20Autograph%20Capsule'
-
+#test_url='https://steamcommunity.com/market/listings/730/London%202018%20Legends%20Autograph%20Capsule'
+#test_url='https://steamcommunity.com/market/listings/730/Paris%202023%20Contenders%20Autograph%20Capsule'
+#test_url='https://steamcommunity.com/market/listings/730/Rio%202022%20Challengers%20Autograph%20Capsule'
+test_url='https://steamcommunity.com/market/listings/730/Rio%202022%20Contenders%20Sticker%20Capsule'
 r = requests.get(test_url).text
 soup = BeautifulSoup(r,'html.parser')
 all_scripts=soup.find_all("script",type='text/javascript')
@@ -43,11 +45,9 @@ df['Day'] = df['Date'].str[4:6]
 df['Month']=df['Month'].map(months)
 df['Day'] = df['Day'].astype(int)
 df=df.drop(columns='Date')
-print(df)
+df.to_csv('Item_data', index=False)
 
 
-plt.plot(df['Price'])
-plt.show()
 
 
 def steamcharts_data(url):
@@ -67,7 +67,7 @@ def steamcharts_data(url):
             month=single_row.find('td',class_='month-cell left')
 
         if(month is not None and avg_players is not None):
-            print(month.text.strip(), avg_players.text.strip())
+            #print(month.text.strip(), avg_players.text.strip())
             row=(month.text.strip(), avg_players.text.strip())
             steamcharts_list.append(row)
 
@@ -79,13 +79,53 @@ def steamcharts_data(url):
     df['Month'] = df['Month'].str.split().str[0]
     df['Month'] = df['Month'].map(months_steamcharts)
     df_reversed = df.iloc[::-1].reset_index(drop=True)
-    print(df_reversed)
+    #print(df_reversed)
     return df_reversed
 
 
 
 URL_steamcharts="https://steamcharts.com/app/730"
 df=steamcharts_data(URL_steamcharts)
-print(df)
+#print(df)
+df.to_csv('Players_count', index=False)
 
 
+
+def merging_data(item_csv, players_csv):
+    item_df=pd.read_csv(item_csv)
+    players_df=pd.read_csv(players_csv)
+
+    sum_sold = sum((item_df['Quantity sold']))
+    sum_sold_in_k=round(sum_sold/1000,2)
+    start_month=item_df['Month'].iloc[0]
+    start_year=item_df['Year'].iloc[0]
+    first_index_row=players_df.loc[(players_df['Month']==start_month) & (players_df['Year']==start_year)]
+    start_index = first_index_row.index[0]
+    players_df = players_df.iloc[start_index:]
+
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Price', color=color)
+    ax1.plot(item_df.index, item_df['Price'], color=color, label='Price')
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    # Tworzenie drugiej osi Y
+    ax2 = ax1.twinx()
+
+    # Wykres dla ilości sprzedanych (Sell) ze skalą logarytmiczną na osi Y
+    color = 'tab:blue'
+    ax2.set_ylabel('Sell', color=color)
+    ax2.plot(item_df.index, item_df['Quantity sold'], color=color, label='Sell')
+    ax2.set_yscale('log')  # Ustawienie skali logarytmicznej na osi Y dla serii 'Sell'
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    title='Sold: '+str(sum_sold_in_k)+'k'
+    # Dodanie legendy
+    fig.tight_layout()
+    plt.legend()
+    plt.suptitle(title, y=0.95)
+    plt.show()
+
+merging_data('Item_data','Players_count')
