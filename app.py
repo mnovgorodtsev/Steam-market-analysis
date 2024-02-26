d74f3ae10584ea8d570719588e4ba76a978b32ec
Matwej Novgorodtsev <92621652+mnovgorodtsev@users.noreply.github.com>
2024-02-27 00:28:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,72 @@
 import tkinter as tk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+from PIL import Image,ImageTk
 
-root = tk.Tk()
-root.title("Wykres w Tkinterze")
-root.geometry("1200x800")
-# Ustawienie geometrii okna
-window_width = 600
-window_height = 400
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = (screen_width / 2) - (window_width / 2)
-y = (screen_height / 2) - (window_height / 2)
-root.geometry(f'{window_width}x{window_height}+{int(x)}+{int(y)}')
+class Aplikacja(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Steam comparison")
 
-# Tworzenie wykresu
-fig = Figure(figsize=(5, 4), dpi=100)
-plot = fig.add_subplot(111)
-x_data = [1, 2, 3, 4, 5]
-y_data = [2, 3, 5, 7, 6]
-plot.plot(x_data, y_data)
+        # Utworzenie widżetu Canvas
+        self.canvas = tk.Canvas(self, width=800, height=600, bg="#e3dbd5")
+        self.canvas.pack()
 
-# Dodanie wykresu do interfejsu Tkinter
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.draw()
-canvas.get_tk_widget().place(relx=0, rely=1, anchor="sw")
+        # Utwórz startową scenę jako domyślną
+        self.startowa_scena = MainMenu(self, self.canvas)  # Przekazanie Canvas jako argument
+        self.startowa_scena.pack()
 
-root.mainloop()
+        # Zmień rozmiar okna
+        self.geometry("800x600")
+
+
+class MainMenu(tk.Frame):
+
+    def przejdz_do_nastepnej_sceny(self):
+        pass
+    def capsules_clicked(self):
+        pass
+    def events_clicked(self):
+        print('you clicked an iamge')
+        self.destroy()
+        self.canvas.destroy()
+        startowa_scena = KolejnaScena(self.master)
+        startowa_scena.pack()
+
+
+    def __init__(self, master, canvas, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.canvas = canvas
+
+
+
+        self.capsule_button = tk.Button(self, text="Przejdź do następnej sceny", command=self.przejdz_do_nastepnej_sceny)
+        self.capsule_button.pack(pady=10)
+        self.capsules_image = tk.PhotoImage(file="icons/capsules.png")
+        events = Image.open("icons/events.png")
+        resized_image = events.resize((315, 315), Image.Resampling.LANCZOS)
+        self.events_image = ImageTk.PhotoImage(resized_image)
+        self.canvas.create_image(10, 50, image=self.capsules_image, anchor="nw")
+        self.canvas.create_image(450, 70, image=self.events_image, anchor="nw")
+        self.canvas.create_text(400, 28, text="Steam prices comparison", fill="black", font=('Helvetica 24 bold'),justify='center')
+        self.canvas.create_text(180, 420, text="Capsules", fill="black", font=('Helvetica 24 bold'))
+        self.canvas.create_text(610, 420, text="Events", fill="black", font=('Helvetica 24 bold'))
+
+        img_label= tk.Label(image=self.capsules_image)
+
+        self.button1 = tk.Button(self.canvas, image=self.capsules_image, command=self.events_clicked)
+        self.canvas.pack()
+
+class KolejnaScena(tk.Frame):
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.przycisk = tk.Button(self, text="Powrót do startowej sceny", command=self.powrot_do_startowej_sceny)
+        self.przycisk.pack(pady=10)
+    def powrot_do_startowej_sceny(self):
+        self.destroy()
+        startowa_scena = MainMenu(self.master)
+        startowa_scena.pack()
+
+
+
+if __name__ == "__main__":
+    aplikacja = Aplikacja()
+    aplikacja.mainloop()
